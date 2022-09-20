288c6a039ff4e35993283a97df58427c45c3de2a
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-20 14:01:26 +0200

    Esercizio con bonus

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ console.log('SCRIPT OK!');
 const app = new Vue({
     el: "#app",
     data: {
-        emails: [1, 2, 1, 3, 2]
+        emails: []
     },
+    mounted() {
+
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    console.log(response.data.response);
+
+                    this.emails.push(response.data.response);
+
+                })
+        }
+
+    }
 })
