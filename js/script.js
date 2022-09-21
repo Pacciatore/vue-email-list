4bb7ed66664de64210c9cbfcbdb97ed239049117
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-21 09:48:08 +0200

    aggiunta di email solo se ottenuta con successo

--- js/script.js
+++ js/script.js
@@ -25,8 +25,9 @@ const app = new Vue({
                         .get('https://flynn.boolean.careers/exercises/api/random/mail')
                         .then((response) => {
                             console.log(response.data.response);
-
-                            this.emails.push(response.data.response);
+                            if (response.status === 200) {
+                                this.emails.push(response.data.response);
+                            }
 
                         })
                 }
