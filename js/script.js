4514e8575f41316132518cf2b6e6a333062b4022
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-09-21 03:54:52 +0200

    creazione array email dimensione fissa in input

--- js/script.js
+++ js/script.js
@@ -8,16 +8,19 @@ const app = new Vue({
     },
     methods: {
         generateEmails() {
-            for (let i = 0; i < this.numOfEmails; i++) {
-                axios
-                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                    .then((response) => {
-                        console.log(response.data.response);
+            this.emails.length = 0;
+            setTimeout(() => {
+                for (let i = 0; i < this.numOfEmails; i++) {
+                    axios
+                        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        .then((response) => {
+                            console.log(response.data.response);
 
-                        this.emails.push(response.data.response);
+                            this.emails.push(response.data.response);
 
-                    })
-            }
+                        })
+                }
+            }, 5)
         },
     }
 })
